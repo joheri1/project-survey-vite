258c2ce8a26c6da50b887e0b9c5215e3ba80932d
iamfannyhenriques <fanny.henriques@hotmail.com>
2024-10-17 14:04:49 +0200

    resolved code in app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App.jsx"
 import "./index.css"
+import "./components/App.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
