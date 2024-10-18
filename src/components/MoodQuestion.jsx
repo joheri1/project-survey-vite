2e9be4a4388139f4cc1659a0285d10821ae5a186
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-10-18 10:53:45 +0200

    Add placeholder text inside input field. Adjust font-sizes, gaps, etc according to the desired design

--- src/components/MoodQuestion.jsx
+++ src/components/MoodQuestion.jsx
@@ -8,18 +8,21 @@ export const MoodQuestion = ({ updateFormData, value }) => {
 
   return (
     <div>
-      <div className="question-container">
-        <h1>1.</h1>
-        <p>What is your favorite mood-boosting song?</p>
+      <div className="form-container">
+        <div className="question-container">
+          <h1>1.</h1>
+          <p>What is your favorite mood-boosting song?</p>
       </div>
-      <div className="answer-container">
-        <input
-          type="text"
-          value={value}
-          onChange={handleMoodQuestionChange}
-        />
+        <div className="answer-container">
+          <input
+            type="text"
+            placeholder="Write your answer here..."
+            value={value}
+            onChange={handleMoodQuestionChange}
+          />
       </div>
     </div>
+  </div>
   );
 };
 
